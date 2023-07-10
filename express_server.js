a209b00af134457835cbaaeea3354f702356e167
oluobamzy <oluobamzy@gmail.com>
2023-07-10 19:17:09 -0400

    redirects working

--- express_server.js
+++ express_server.js
@@ -1,6 +1,22 @@
 const express = require('express');
 const app = express();
 const PORT = 8080;
+app.use(express.urlencoded({ extended: true }));
+
+const generateRandomStrings = ()=>{
+ 
+    let result = '';
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    const charactersLength = 6;
+  
+    for (let i = 0; i < charactersLength; i++) {
+      const randomIndex = Math.floor(Math.random() * charactersLength);
+      result += characters.charAt(randomIndex);
+    }
+  
+    return result;
+  
+};
 
 const urlDatabase = {
   "b2xVn2" : "http://www.lighthouselabas.ca",
@@ -14,10 +30,27 @@ app.get("/urls.json",(req,res)=>{
 app.get("/urls",(req,res)=>{
   res.render("urls_index",templateVars);
 });
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+app.post("/urls", (req, res) => {
+  // console.log(req.body); // Log the POST request body to the console
+   let newId = generateRandomStrings();
+   urlDatabase[newId] = req.body["longURL"];
+   console.log(req.body);
+   console.log(urlDatabase);
+   res.redirect(`/urls/${newId}`);
+ });
+ app.get("/u/:id", (req, res) => {
+  // const longURL = ...
+  const longURL = urlDatabase[req.params.id]
+  res.redirect(longURL);
+});
 app.get("/urls/:id",(req,res)=>{
   const templateVars = {id:req.params.id, longURL : urlDatabase[req.params.id]};
   res.render('urls_show.ejs',templateVars);
 });
+
 
 app.listen(PORT, ()=>{
   console.log(`Example app listening on port ${PORT}!`)
