42c34f944f73a87b16ab07edc9bb74cda0449a5c
oluobamzy <oluobamzy@gmail.com>
2023-07-10 15:46:38 -0400

    partials added

--- express_server.js
+++ express_server.js
@@ -7,20 +7,18 @@ const urlDatabase = {
   "9sm5xk": "http://www.google.ca"
 };
 const templateVars = {urls: urlDatabase};
-app.set("view engine", "ejs")
-
-app.get("/",(req,res)=>{
-  res.send("Hello!");
-});
-app.get("/hello",(req,res)=>{
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
-});
+app.set("view engine", "ejs");
 app.get("/urls.json",(req,res)=>{
   res.json(urlDatabase);
 });
 app.get("/urls",(req,res)=>{
   res.render("urls_index",templateVars);
-})
+});
+app.get("/urls/:id",(req,res)=>{
+  const templateVars = {id:req.params.id, longURL : urlDatabase[req.params.id]};
+  res.render('urls_show.ejs',templateVars);
+});
+
 app.listen(PORT, ()=>{
   console.log(`Example app listening on port ${PORT}!`)
 })
