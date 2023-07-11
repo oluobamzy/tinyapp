9efdbeb541e6b7c5a7d22c88e5fa6773f6b28497
oluobamzy <oluobamzy@gmail.com>
2023-07-11 14:13:40 -0400

    delete button added and working

--- express_server.js
+++ express_server.js
@@ -50,6 +50,11 @@ app.get("/urls/:id",(req,res)=>{
   const templateVars = {id:req.params.id, longURL : urlDatabase[req.params.id]};
   res.render('urls_show.ejs',templateVars);
 });
+app.post("/urls/:id/delete",(req,res)=>{
+  let id = req.params.id;
+  delete urlDatabase[id];
+  res.redirect("/urls");
+});
 
 
 app.listen(PORT, ()=>{
